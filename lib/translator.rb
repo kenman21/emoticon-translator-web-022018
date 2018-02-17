bb2b6e2730e7bbbf134abbc200609a3b8f8fed0a
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-17 03:41:42 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,11 +6,13 @@ def load_library(path)
   out = {}
   get_meaning_val = {}
   get_emoticon_val = {}
+  jap.each do |meanings, emoticons|
+    get_meaning_val[emoticons[1]] = meanings 
+    get_emoticon_val[emoticons[0]] = emoticons[1]
+  end 
+  out = {get_meaning => get_meaning_val, get_emoticon => get_emoticon_val}
   out
-  jap
 end
-
-load_library(/emoticons.yml)
 
 def get_japanese_emoticon
   # code goes here
